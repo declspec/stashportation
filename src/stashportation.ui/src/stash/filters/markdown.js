f91251656c18585d46cfc3f0ff97d5e1e989e4ea
Jason Larke <jasonlarke@live.com.au>
2017-03-20 22:53:10 +0800

    Added more language aliases

--- src/stashportation.ui/src/stash/filters/markdown.js
+++ src/stashportation.ui/src/stash/filters/markdown.js
@@ -2,16 +2,17 @@ import markdownit from 'markdown-it';
 import hljs from 'highlight.js';
 
  // Configure some shorthand overrides
-const LANG_SHORTCUTS = {
-    "js": "javascript",
-    "html": "xml"
+const LANGUAGE_ALIASES = {
+    'js': 'javascript',
+    'html': 'xml',
+    'c#': 'cs'
 };
 
 const MD = markdownit({
     html: true,
     highlight: function(code, lang) {
-        if (LANG_SHORTCUTS.hasOwnProperty(lang))
-            lang = LANG_SHORTCUTS[lang];
+        if (LANGUAGE_ALIASES.hasOwnProperty(lang))
+            lang = LANGUAGE_ALIASES[lang];
 
         if (lang && hljs.getLanguage(lang)) {
             try { return hljs.highlight(lang, code).value; }
