673bc036fd26bdf4a2e04639ae7f10e7005fe6f0
Jason Larke <jasonlarke@live.com.au>
2017-03-22 23:40:44 +0800

    Broken AF saving

--- src/stashportation.api/src/stash.js
+++ src/stashportation.api/src/stash.js
@@ -4,18 +4,22 @@ const Tables = require('./config/tables');
 const ValidationError = require('./errors/validation');
 const NotFoundError = require('./errors/not-found');
 
-const SELECT_FULL_SQL = `SELECT id, title, content, serialized_tags, date_created, date_modified FROM ${Tables.Stashes}`;
-const SELECT_SUMMARY_SQL = `SELECT id, title, serialized_tags FROM ${Tables.Stashes}`;
+const SELECT_FULL_SQL = `SELECT s.id, s.title, s.content, s.serialized_tags, s.date_created, s.date_modified FROM ${Tables.Stashes} s`;
+const SELECT_SUMMARY_SQL = `SELECT s.id, s.title, s.serialized_tags FROM ${Tables.Stashes} s`;
 
+const INSERT_STASH_TAGS_SQL = `INSERT INTO ${Tables.StashTags} (stash_id, tag_id) VALUES (?,?)`;
+
+const INSERT_SQL = `INSERT INTO ${Tables.Stashes} (title, content, serialized_tags) VALUES(?,?,?)`;
 const UPDATE_SQL = `
     UPDATE ${Tables.Stashes}
     SET title = ?,
         content = ?,
         serialized_tags = ?,
         date_modified = CURRENT_TIMESTAMP
+    WHERE id = ?
     `;
 
-const INSERT_SQL = `INSERT INTO ${Tables.Stashes} (title, content, serialized_tags) VALUES(?,?,?)`;
+
 
 module.exports = function(tagService, db, config) {
     return new StashService(tagService, db);
@@ -28,40 +32,39 @@ class StashService {
     }
 
     create(stash) {
-        return this.$validate(stash)
-            .then(() => this.$tagService.findOrCreate(stash.tags))
-            .then(tags => {
-                console.log(tags);
-                return this.$db.run(INSERT_SQL, [ stash.title, stash.content, JSON.stringify(stash.tags) ])
-                    .then(stmt => stmt.lastID);
-            });
+        return this.$validate(stash).then(() => Promise.all([
+            this.$tagService.findOrCreate(stash.tags),
+            this.$db.run(INSERT_SQL, [ stash.title, stash.content, JSON.stringify(stash.tags) ])
+        ])).then(([ tags, res ]) => {
+            stash.id = res.lastID;
+            return this.$mapTags(stash, [], tags);
+        }).then(() => stash.id);
     }
 
     update(id, stash) {
         return this.$validate(stash)
             .then(() => this.findById(id))
             .then(ensureExists)
-            .then(old => {
-                return this.$tagService.findOrCreate(stash.tags).then(tags => {
-                    console.log(tags);
-                    return this.$db.run(UPDATE_SQL, [ stash.title, stash.content, JSON.stringify(stash.tags) ]);
-                });
-            });
+            .then(old => Promise.all([
+                this.$db.run(UPDATE_SQL, [ stash.title, stash.content, JSON.stringify(stash.tags), stash.id ]),
+                this.$tagService.findOrCreate(stash.tags)
+                    .then(tags => this.$mapTags(stash, old.tags, tags))
+            ]));
     }
 
     findById(id) {
-        return this.$findWhere('id = ?', id)
+        return this.$findWhere('s.id = ?', id)
             .then(rows => rows.length === 1 ? rows[0] : null);
     }
 
     findByQuery(query) {
-        query = query.replace(/([%_])/g, '\\$1');
-        return this.$findSummariesWhere(`title LIKE ? ESCAPE '\\'`, query);
+        query = '%' + query.replace(/([%_])/g, '\\$1') + '%';
+        return this.$findSummariesWhere(`s.title LIKE ?`, query);
     }
 
     findByTag(tag) {
         tag = tag.toLowerCase();
-        return this.$findSummariesWhere(`EXISTS(SELECT 1 FROM ${Tables.StashTags} st INNER JOIN ${Tables.Tags} t ON t.id = st.tag_id WHERE st.stash_id = id)`);
+        return this.$findSummariesWhere(`EXISTS(SELECT 1 FROM ${Tables.StashTags} st INNER JOIN ${Tables.Tags} t ON t.id = st.tag_id WHERE st.stash_id = s.id AND t.tag = ?)`, tag);
     }
 
     $findWhere(condition, params) {
@@ -74,18 +77,62 @@ class StashService {
             .then(mapRows);
     }    
 
-    $validate(stash) {
-        if (stash.content && typeof(stash.content) !== 'string')
-            return Promise.reject(new ValidationError('Invalid stash content provided'));
+    $mapTags(stash, existingTags, allTags) {
+        const refs = {};
 
-        if (stash.title) {
-            if (typeof(stash.title) !== 'string')
-                return Promise.reject(new ValidationError('Invalid stash title provided'));
+        stash.tags.forEach(t => refs[t] = (refs[t]||0)+1);
+        existingTags.forEach(t => refs[t] = (refs[t]||0)-1);
 
-            if (stash.title.length > 500)
-                return Promise.reject(new ValidationError('Stash title must be less than 500 characters'));
+        const tagLookup = {};
+        allTags.forEach(t => tagLookup[t.tag] = t.id);
+
+        let kept = [],
+            promises = [],
+            deleted = 0;
+
+        for(var tag in refs) {
+            const count = refs[tag];
+            if (count === 0) 
+                kept.push(tagLookup[tag]);
+            else if (count < 0)
+                deleted++;
+            else {
+                promises.push(this.$db.run(INSERT_STASH_TAGS_SQL, [ stash.id, tagLookup[tag] ])
+                    .then(res => res.lastID));
+            }
         }
 
+        return Promise.all(promises).then(results => {
+            if (deleted === 0)
+                return;
+
+            kept = kept.concat(results);
+            kept.push(stash.id);
+
+            const deleteSql = kept.length > 1
+                ? `DELETE FROM ${Tables.StashTags} WHERE tag_id NOT IN (${Array(kept.length-1).fill('?').join()}) AND stash_id = ?`
+                : `DELETE FROM ${Tables.StashTags} WHERE stash_id = ?`;
+
+            return this.$db.run(deleteSql, kept);
+        });
+    }
+
+    $validate(stash) {
+        if (!stash.content)
+            return Promise.reject(new ValidationError('Stash content missing'));
+        
+        if (typeof(stash.content) !== 'string')
+            return Promise.reject(new ValidationError('Invalid stash content provided'));
+
+        if (!stash.title) 
+            return Promise.reject(new ValidationError('Stash title missing'));
+
+        if (typeof(stash.title) !== 'string')
+            return Promise.reject(new ValidationError('Invalid stash title provided'));
+
+        if (stash.title.length > 500)
+            return Promise.reject(new ValidationError('Stash title must be less than 500 characters'));
+        
         if (stash.tags) {
             if (!Array.isArray(stash.tags))
                 return Promise.reject('Invalid stash tags provided');
--- src/stashportation.ui/src/stash/services/stash.js
+++ src/stashportation.ui/src/stash/services/stash.js
@@ -1,73 +1,36 @@
 import { inject } from 'core';
 
-const REQUEST_LATENCY = 1000;
-
-@inject('$q', '$timeout', 'WebApiService')
+@inject('WebApiService')
 export class StashService {
-    constructor(q, delay, webApiService) {
-        this.promise = q;
+    constructor( webApiService) {
         this.webApi = webApiService;
-        this.delay = delay;
-
-        this.$loadCache();
-        this.$$id = Object.keys(this.stashCache).reduce((maxId, id) => Math.max(maxId, id), 0);
     }
 
     create(stash) {
         return this.webApi.post('/api/stash', stash).then(res => res.data);
-
-
-        stash.id = ++this.$$id;
-
-        this.stashCache[stash.id] = stash;
-        this.$saveCache();
-
-        return this.promise.when(stash.id);
     }
 
     save(stash) {
         return this.webApi.put(`/api/stash/${stash.id}`, stash);
-
-        if (this.stashCache[stash.id] !== stash)
-            this.stashCache[stash.id] = stash;
-            
-        this.$saveCache();
-        return this.promise.when();
     }
 
     findById(id) {
-        return this.delay(() => this.stashCache[id] || null, REQUEST_LATENCY);
+        return this.webApi.get(`/api/stash/${encodeURIComponent(id)}`)
+            .then(res => res.status === 200 ? res.data : null);
     }
 
     findByQuery(query) {
-        const lowerQuery = query.toLowerCase();
-        return this.delay(() => Object.values(this.stashCache).filter(s => s.title.toLowerCase().indexOf(lowerQuery) >= 0), REQUEST_LATENCY);
+        return this.webApi.get(`/api/stash?q=${encodeURIComponent(query)}`)
+            .then(res => res.data);
     }
 
     findByTag(tag) {
-        const lowerTag = tag.toLowerCase();
-        return this.delay(() => Object.values(this.stashCache).filter(s => s.tags.indexOf(lowerTag) >= 0), REQUEST_LATENCY);
+        return this.webApi.get(`/api/stash/tags/${encodeURIComponent(tag.toLowerCase())}`)
+            .then(res => res.data);
     }
 
-    findAll() {
-        return this.delay(() => Object.values(this.stashCache), REQUEST_LATENCY);
-    }
-
-    getAllTags() {
-        return this.promise.when([
-            "javascript","java","c#","php","android","jquery","python","html","c++",
-            "ios","css","mysql","sql","asp.net","objective-c","ruby-on-rails",".net",
-            "c","angularjs","arrays","iphone","sql-server","json","ruby","r","ajax",
-            "node.js","regex","xml","asp.net-mvc","linux","swift","django","wpf",
-            "database","excel"
-        ]);
-    }
-
-    $loadCache() {
-        this.stashCache = JSON.parse(sessionStorage.getItem('stashportation::stashCache') || '{}');
-    }
-
-    $saveCache() {
-        sessionStorage.setItem('stashportation::stashCache', JSON.stringify(this.stashCache));
+    findAllTags() {
+        return this.webApi.get(`/api/tags`)
+            .then(res => res.data);
     }
 }
