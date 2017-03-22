037c1ed15cbc760c4fefbb9ff0bd07d1b146d34b
Jason Larke <jason.larke@curtin.edu.au>
2017-03-22 12:18:12 +0800

    Updated pluralize filter to return undefined when count is not yet defined This makes it work correctly with one-time bindings when the value is still being computed (one-time bindings only bind once the value is no longer undefined)

--- src/stashportation.ui/src/core-ui/filters/pluralize.js
+++ src/stashportation.ui/src/core-ui/filters/pluralize.js
@@ -1,4 +1,7 @@
 export function PluralizeFilter(count, singular) {
+    if ('undefined' === typeof(count))
+        return undefined;
+        
     const num = count|0;
     return num+' '+(count === 1 ? singular : singular + 's');
 }
