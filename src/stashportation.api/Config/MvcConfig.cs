cdf45e976b5c467e570352a24838473fb8e324f9
Jason Larke <jasonlarke@live.com.au>
2017-03-28 22:08:40 +0800

    Added data annotation validation

--- src/stashportation.api/Config/MvcConfig.cs
+++ src/stashportation.api/Config/MvcConfig.cs
@@ -3,7 +3,8 @@
 namespace Stashportation.Config {
     public static class MvcConfig {
         public static void ConfigureMvc(this IServiceCollection services) {
-            var builder = services.AddMvcCore();
+            var builder = services.AddMvcCore()
+                .AddDataAnnotations();
 
             builder.AddJsonFormatters(settings => {
                 settings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
--- src/stashportation.api/Models/Stash.cs
+++ src/stashportation.api/Models/Stash.cs
@@ -4,15 +4,15 @@ using System.ComponentModel.DataAnnotations;
 namespace Stashportation.Models {
     public class StashSummary {
         public long Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Every stash must have a title")]
         public string Title { get; set; }
-        [Required]
+        [Required(ErrorMessage = "At least one tag must be specified for a stash")]
         [MaxLength(50, ErrorMessage = "A maximum of 50 tags can be set on a stash")]
         public IList<string> Tags { get; set; }
     }
 
     public class Stash : StashSummary {
-        [Required]
+        [Required(ErrorMessage = "Must provide some content in the stash"]
         public string Content { get; set; }
     }
 }
